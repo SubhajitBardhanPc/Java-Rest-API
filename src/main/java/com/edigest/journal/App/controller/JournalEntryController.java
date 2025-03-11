67092a240bd7c67c7a6121d3a156e8dd5fa42d08
bardhansubhajit3@gmail.com <bardhansubhajit3@gmail.com>
2025-03-11 08:21:57 +0530

    My code is Working

--- src/main/java/com/edigest/journal/App/controller/JournalEntryController.java
+++ src/main/java/com/edigest/journal/App/controller/JournalEntryController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+//Working Fine
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
